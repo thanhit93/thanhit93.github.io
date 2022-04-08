fb1e5a54cc667dde813133c7fc39a57cb501928a
Thanh Truong <truongthanhcn12@gmail.com>
2022-04-08 08:53:29 +0700

    Update build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f05a54e994765143342169da9da22e94",
-"index.html": "a1378010951ac395dad335796a9e13f0",
-"/": "a1378010951ac395dad335796a9e13f0",
-"main.dart.js": "59aa641fbc771f010982481f6763dce1",
+"index.html": "30b3c7ff834338450c3bcdf87ca0ca2e",
+"/": "30b3c7ff834338450c3bcdf87ca0ca2e",
+"main.dart.js": "14f2e2efc21c20a910dc797b03188d01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
